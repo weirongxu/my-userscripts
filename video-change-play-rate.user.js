3308f8d7a9a03d60e4d5cebaea4c5c1f9aa3d7f4
Weirong Xu <weirongxu.raidou@gmail.com>
2017-11-06 10:57:43 +0800

    add video info hint

--- video-change-play-rate.user.js
+++ video-change-play-rate.user.js
@@ -9,37 +9,63 @@
 // ==/UserScript==
 
 (function() {
-'use strict';
+'use strict'
 
-var script = document.createElement("script");
-script.setAttribute("src", "//code.jquery.com/jquery-2.2.4.min.js");
+var script = document.createElement("script")
+script.setAttribute("src", "//code.jquery.com/jquery-2.2.4.min.js")
 script.addEventListener('load', () => {
-  main(jQuery.noConflict(true));
-}, false);
-document.body.appendChild(script);
+  main(jQuery.noConflict(true))
+}, false)
+document.body.appendChild(script)
+
+function showInfo($video, info) {
+  video = $video[0]
+  const rect = video.getBoundingClientRect()
+  const id = 'video-change-play-rate-info'
+  let $info = $(`#${id}`)
+  if (! $info.length) {
+    $info = $('body').append(`<div id="${id}"></div>`).find(`#${id}`)
+  }
+  $info.css({
+    background: 'rgba(0, 0, 0, 0.5)',
+    color: 'white',
+    padding: '5px',
+    zIndex: 1000000000,
+    borderRadius: '5px',
+  })
+  $info.html(info)
+  $info.css({
+    position: 'fixed',
+    left: rect.left + video.clientWidth/2 - $info.width()/2 + 'px',
+    top: rect.top + video.clientHeight/2 - $info.height()/2 + 'px',
+  })
+}
 
 function main($) {
   $(document).on('keydown', (e) => {
     if (e.ctrlKey) {
-      const $video = $('video');
+      const $video = $('video')
       switch (e.key.toLowerCase()) {
         case '[':
           $video.each(function() {
-            this.playbackRate -= 0.05;
-          });
-          return false;
+            this.playbackRate -= 0.05
+            showInfo($video, `rate: ${this.playbackRate.toFixed(2)}`)
+          })
+          return false
         case ']':
           $video.each(function() {
-            this.playbackRate += 0.05;
-          });
-          return false;
+            this.playbackRate += 0.05
+            showInfo($video, `rate: ${this.playbackRate.toFixed(2)}`)
+          })
+          return false
         case 'backspace':
           $video.each(function() {
             this.playbackRate = 1
-          });
-          return false;
+            showInfo($video, `rate: ${this.playbackRate.toFixed(2)}`)
+          })
+          return false
       }
     }
-  });
+  })
 }
-})();
+})()
