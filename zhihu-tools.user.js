c834f5e6d41e6e07836f953f995e6f79d88f9a74
Weirong Xu <weirongxu.raidou@gmail.com>
2017-11-08 00:16:30 +0800

    update zhihu tools

--- zhihu-tools.user.js
+++ zhihu-tools.user.js
@@ -5,42 +5,102 @@
 // @description  zhihu tools
 // @author       Raidou
 // @match        *://*.zhihu.com/*
-// @grant        none
+// @grant        GM_xmlhttpRequest
 // ==/UserScript==
 
 (function() {
 'use strict'
 
-var script = document.createElement("script")
-script.setAttribute("src", "//code.jquery.com/jquery-2.2.4.min.js")
-script.addEventListener('load', () => {
-  main(jQuery.noConflict(true))
-}, false)
-document.body.appendChild(script)
+const fnName = `ZhihuTools_${Number(new Date())}`
 
-async function main($) {
-  const config = JSON.parse($('#clientConfig').val())
-  const headers = {
-    'Authorization': `Bearer ${config.tokens['Authorization'].join('|')}`,
-    'X-UDID': config.tokens['X-UDID'],
-    'X-XSRF-TOKEN': config.tokens['X-XSRF-TOKEN'],
-    'Accept': 'application/json',
-    'Content-Type': 'application/json',
+GM_xmlhttpRequest({
+  method: 'GET',
+  url: "https://code.jquery.com/jquery-2.2.4.min.js",
+  onload(res) {
+    console.log(res.response)
+    const script = document.createElement("script")
+    script.text = `${res.response};${fnName}(jQuery.noConflict(true));`
+    document.body.appendChild(script)
+  },
+})
+
+// var script = document.createElement('script')
+// script.setAttribute("src", "//code.jquery.com/jquery-2.2.4.min.js")
+// script.addEventListener('load', () => {
+//   main(jQuery.noConflict(true))
+// }, false)
+// document.body.appendChild(script)
+
+class Zhihu {
+  constructor($) {
+    this.$ = $
+
+    this.config = JSON.parse($('#clientConfig').val())
+    this.headers = {
+      'Authorization': `Bearer ${this.config.tokens['Authorization'].join('|')}`,
+      'X-UDID': this.config.tokens['X-UDID'],
+      'X-XSRF-TOKEN': this.config.tokens['X-XSRF-TOKEN'],
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    }
   }
-  if (location.host === 'zhuanlan.zhihu.com') {
+
+  async get(url) {
+    const res = await fetch(url, {
+      method: 'GET',
+      headers: this.headers,
+    })
+    return await res.json()
+  }
+
+  async post(url, body) {
+    const res = await fetch(url, {
+      method: 'POST',
+      headers: this.headers,
+      body: JSON.stringify(body)
+    })
+    return await res.json()
+  }
+
+  boot() {
+    console.dir(location.host)
+    switch (location.host) {
+      case 'www.zhihu.com':
+        this.home(this.$)
+        break
+      case 'zhuanlan.zhihu.com':
+        this.zhuanlan(this.$)
+        break
+    }
+  }
+
+  async home($) {
+    const $menu = $('.Menu.PushNotifications-menu')
+    const observer = new MutationObserver(function(mutations) {
+      mutations.forEach(function(mutation) {
+        console.log(mutation.type)
+      })
+    })
+    console.dir($menu[0])
+    observer.observe($menu[0], {
+      attributes: true,
+      childList: true,
+      characterData: true,
+    })
+    $('.PushNotifications-list .PushNotifications-item').each(function() {
+      $(this).append('<span class="UserLink">Read later</span>')
+    })
+  }
+
+  async zhuanlan($) {
     const match = location.pathname.match(/.*\/(.*)/)
     if (match[1]) {
       const articleId = match[1]
       const readLayterId = 183046846
-      const collecteds = await (await fetch(
-        `https://www.zhihu.com/api/v4/articles/${articleId}/relations/collected?favlist_ids=[${readLayterId}]`,
-        {
-          headers,
-        }
-      )).json()
+      const collecteds = await this.get(`https://www.zhihu.com/api/v4/articles/${articleId}/relations/collected?favlist_ids=[${readLayterId}]`)
       const collected = collecteds[0].collected
       let css = `
-      padding: 20px;
+      padding: 18px;
       cursor: pointer;
       `
       const $laterBtn = $('.Navbar-functionality').prepend(
@@ -55,14 +115,10 @@ async function main($) {
         btnCollected()
       } else {
         $laterBtn.on('click', async () => {
-          const data = await (await fetch(`https://www.zhihu.com/api/v4/favlists/${readLayterId}/items`, {
-            method: 'POST',
-            headers,
-            body: JSON.stringify({
-              content_id: articleId,
-              content_type: "article",
-            })
-          })).json()
+          const data = await this.post(`https://www.zhihu.com/api/v4/favlists/${readLayterId}/items`, {
+            content_id: articleId,
+            content_type: "article",
+          })
           if (data.content) {
             btnCollected()
           }
@@ -70,6 +126,12 @@ async function main($) {
       }
     }
   }
+}
+
+window[fnName] = ($) => {
+  console.dir('test')
+  const zhihu = new Zhihu($)
+  zhihu.boot()
 }
 
 })()
