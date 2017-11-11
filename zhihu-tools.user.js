fce39660899f1c5efcba610fe16542c21b26a58c
Weirong Xu <weirongxu.raidou@gmail.com>
2017-11-11 16:17:01 +0800

    update zhihu tools

--- zhihu-tools.user.js
+++ zhihu-tools.user.js
@@ -33,23 +33,24 @@ function XHR(options) {
     jsCookie,
   ] = await Promise.all([
     'https://code.jquery.com/jquery-2.2.4.min.js',
-    'https://cdnjs.cloudflare.com/ajax/libs/js-cookie/2.2.0/js.cookie.min.js',
   ].map(url => XHR({
     method: 'GET',
     url,
   })))
   eval(`
     ${jquery.response};
-    ${jsCookie.response};
-    ${fnName}(jQuery.noConflict(true), Cookies);
+    ${fnName}(jQuery.noConflict(true));
   `)
 })()
 
 
 class Zhihu {
-  constructor($, Cookies) {
+  constructor($) {
     this.$ = $
-    this.Cookies = Cookies
+    this.headers = {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    }
   }
 
   async get(url) {
@@ -69,31 +70,39 @@ class Zhihu {
     return await res.json()
   }
 
-  async boot() {
-    // console.dir($('#data'))
-    // zhihu remove #data state dom when load page
-
-    // console.dir(Cookies.get())
-    // console.dir(document.cookie)
-    const $ = this.$
-    // const html = await (await fetch('https://www.zhihu.com', {
-    //   credentials: 'same-origin'
-    // })).text()
-    // console.log(html)
-    // const state = $(html).find('#data').attr('data-state')
-    // console.dir(state)
-    this.config = JSON.parse($('#clientConfig').val())
-    this.headers = {
-      // z_c0
-      'Authorization': `Bearer ${this.config.tokens['Authorization'].join('|')}`,
-      // d_c0
-      'X-UDID': this.config.tokens['X-UDID'],
-      // _xsrf
-      'X-XSRF-TOKEN': this.config.tokens['X-XSRF-TOKEN'],
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
+  watch($dom, selector, once, callback) {
+    if ($dom.length) {
+      const done = ($target) => {
+        if (once === true) {
+          observer.disconnect()
+        }
+        callback($target)
+      }
+      const observer = new MutationObserver((mutations) => {
+        mutations.forEach((mutation) => {
+          if (selector) {
+            let $target = this.$(mutation.addedNodes[0]).filter(selector)
+            if (! $target.length) {
+              $target = this.$(mutation.addedNodes[0]).find(selector)
+            }
+            if ($target.length) {
+              done($target)
+            }
+          } else {
+            done()
+          }
+        })
+      })
+      $dom.each(function() {
+        observer.observe(this, {
+          childList: true,
+        })
+      })
     }
-    console.dir(location.host)
+  }
+
+  async boot() {
+    const $ = this.$
     switch (location.host) {
       case 'www.zhihu.com':
         this.home($)
@@ -104,54 +113,107 @@ class Zhihu {
     }
   }
 
+  get readLayterId() {
+    return 183046846
+  }
+  async inReadLater(articleId) {
+    const collecteds = await this.get(`https://www.zhihu.com/api/v4/articles/${articleId}/relations/collected?favlist_ids=[${this.readLayterId}]`)
+    return collecteds[0].collected
+  }
+
+  async addToReadLater(articleId) {
+    const data = await this.post(`https://www.zhihu.com/api/v4/favlists/${this.readLayterId}/items`, {
+      content_id: articleId,
+      content_type: 'article',
+    })
+    if (data.content) {
+      return data.content
+    } else {
+      throw Error('read later error')
+    }
+  }
+
+  matchArticleId(url) {
+    if (! url.toString().startsWith('http')) {
+      url = `http:${url}`
+    }
+    const match = (new URL(url)).pathname.match(/.*\/(.*)/)
+    return match[1]
+  }
+
+  btnCollected($btn) {
+    $btn.css({
+      color: 'white',
+      background: '#ccc',
+    })
+  }
+
   async home($) {
-    const $menu = $('.Menu.PushNotifications-menu')
-    const observer = new MutationObserver(function(mutations) {
-      mutations.forEach(function(mutation) {
-        console.log(mutation.type)
+    const html = await (await fetch('https://www.zhihu.com', {
+      credentials: 'same-origin'
+    })).text()
+    const state = JSON.parse($(html).filter('#data').attr('data-state'))
+    this.headers = {
+      ...{
+        'Authorization': `Bearer ${state.token.carCompose}`,
+        'X-UDID': state.token.xUDID,
+        'X-XSRF-TOKEN': state.token.xsrf,
+      },
+    }
+
+    this.watch($('body'), '.Menu.PushNotifications-menu', false, ($menu) => {
+      const $list = $menu.find('.PushNotifications-list')
+      this.watch($list, '.PushNotifications-item', false, async ($item) => {
+        const href = $item.find('> span:nth-child(3) a').attr('href')
+        if (href.toString().includes('zhuanlan')) {
+          $item.append('<span class="read-later Button">Read later</span>')
+          const articleId = this.matchArticleId(href)
+          const $laterBtn = $item.find('.read-later')
+          $laterBtn.css({
+            padding: '5px',
+            lineHeight: '14px',
+            margin: '0 10px',
+          })
+          if (await this.inReadLater(articleId)) {
+            this.btnCollected($laterBtn)
+          } else {
+            $laterBtn.on('click', async () => {
+              await this.addToReadLater(articleId)
+              this.btnCollected($laterBtn)
+            })
+          }
+        }
       })
-    })
-    console.dir($menu[0])
-    observer.observe($menu[0], {
-      attributes: true,
-      childList: true,
-      characterData: true,
-    })
-    $('.PushNotifications-list .PushNotifications-item').each(function() {
-      $(this).append('<span class="UserLink">Read later</span>')
     })
   }
 
   async zhuanlan($) {
-    const match = location.pathname.match(/.*\/(.*)/)
-    if (match[1]) {
-      const articleId = match[1]
-      const readLayterId = 183046846
-      const collecteds = await this.get(`https://www.zhihu.com/api/v4/articles/${articleId}/relations/collected?favlist_ids=[${readLayterId}]`)
-      const collected = collecteds[0].collected
+    this.config = JSON.parse($('#clientConfig').val())
+    this.headers = {
+      ...{
+        // z_c0
+        'Authorization': `Bearer ${this.config.tokens['Authorization'].join('|')}`,
+        // d_c0
+        'X-UDID': this.config.tokens['X-UDID'],
+        // _xsrf
+        'X-XSRF-TOKEN': this.config.tokens['X-XSRF-TOKEN'],
+      },
+    }
+
+    const articleId = this.matchArticleId(location)
+    if (articleId) {
       let css = `
       padding: 18px;
       cursor: pointer;
       `
       const $laterBtn = $('.Navbar-functionality').prepend(
         `<a class="read-later" style="${css}">Read later</a>`).find('.read-later')
-      const btnCollected = () => {
-        $laterBtn.css({
-          color: 'white',
-          background: '#ccc',
-        })
-      }
-      if (collected) {
-        btnCollected()
+      if (await this.inReadLater(articleId)) {
+        this.btnCollected($laterBtn)
       } else {
         $laterBtn.on('click', async () => {
-          const data = await this.post(`https://www.zhihu.com/api/v4/favlists/${readLayterId}/items`, {
-            content_id: articleId,
-            content_type: "article",
-          })
-          if (data.content) {
-            btnCollected()
-          }
+          await this.addToReadLater(articleId)
+          this.btnCollected($laterBtn)
         })
       }
     }
