3c2d8f9394912c4442e23068021360984d740568
Weirong Xu <weirongxu.raidou@gmail.com>
2018-12-04 22:39:46 +0800

    support picture in picture

--- userscripts/video-controller.user.js
+++ userscripts/video-controller.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         video controller
 // @namespace    https://github.com/weirongxu/my-userscripts
-// @version      0.3.3
+// @version      0.4.0
 // @description  video controller
 // @author       Raidou
 // @match        *://*/*
@@ -9,24 +9,24 @@
 // ==/UserScript==
 
 (function() {
-  'use strict'
+  'use strict';
 
   function setStyle($node, css) {
     Object.entries(css).forEach(([key, val]) => {
-      $node.style[key] = val
-    })
+      $node.style[key] = val;
+    });
   }
 
-  let infoTimer = null
+  let infoTimer = null;
   function showInfo(video, info) {
-    const rect = video.getBoundingClientRect()
-    const cls = 'video-controller-info'
-    const $roots = document.querySelectorAll('body, :-webkit-full-screen')
-    const $root = $roots[$roots.length-1]
-    let $info = $root.querySelector(`.${cls}`)
+    const rect = video.getBoundingClientRect();
+    const cls = 'video-controller-info';
+    const $roots = document.querySelectorAll('body, :-webkit-full-screen');
+    const $root = $roots[$roots.length-1];
+    let $info = $root.querySelector(`.${cls}`);
     if (! $info) {
-      $root.insertAdjacentHTML('beforeend', `<div class="${cls}"></div>`)
-      $info = $root.querySelector(`.${cls}`)
+      $root.insertAdjacentHTML('beforeend', `<div class="${cls}"></div>`);
+      $info = $root.querySelector(`.${cls}`);
     }
     setStyle($info, {
       background: 'rgba(0, 0, 0, 0.5)',
@@ -38,66 +38,74 @@
       position: 'fixed',
       opacity: 0,
       transition: '.5s opacity',
-    })
-    $info.innerHTML = info
-    $info.style.left = rect.left + video.clientWidth/2 - $info.clientWidth/2 + 'px'
-    $info.style.top = rect.top + video.clientHeight/2 - $info.clientHeight/2 + 'px'
-    $info.style.opacity = 1
-    clearTimeout(infoTimer)
+    });
+    $info.innerHTML = info;
+    $info.style.left = rect.left + video.clientWidth/2 - $info.clientWidth/2 + 'px';
+    $info.style.top = rect.top + video.clientHeight/2 - $info.clientHeight/2 + 'px';
+    $info.style.opacity = 1;
+    clearTimeout(infoTimer);
     infoTimer = setTimeout(() => {
-      $info.style.opacity = 0
-    }, 600)
+      $info.style.opacity = 0;
+    }, 600);
   }
 
   function videoInView() {
-    const $video = document.querySelectorAll('video')
-    let maxVisibleArea = 0
-    let video = null
+    const $video = document.querySelectorAll('video');
+    let maxVisibleArea = 0;
+    let video = null;
     $video.forEach((_video) => {
-      const rect = _video.getBoundingClientRect()
-      const left = Math.max(rect.left, 0)
-      const top = Math.max(rect.top, 0)
-      const right = Math.min(rect.right, window.innerWidth)
-      const bottom = Math.min(rect.bottom, window.innerHeight)
-      const visibleArea = (right-left) * (bottom-top)
+      const rect = _video.getBoundingClientRect();
+      const left = Math.max(rect.left, 0);
+      const top = Math.max(rect.top, 0);
+      const right = Math.min(rect.right, window.innerWidth);
+      const bottom = Math.min(rect.bottom, window.innerHeight);
+      const visibleArea = (right-left) * (bottom-top);
       if (visibleArea > maxVisibleArea) {
-        maxVisibleArea = visibleArea
-        video = _video
+        maxVisibleArea = visibleArea;
+        video = _video;
       }
-    })
-    return video
+    });
+    return video;
   }
 
   document.addEventListener('keydown', (e) => {
     if (['INPUT', 'TEXTAREA'].includes(e.target.tagName)) {
-      return
+      return;
     }
     const stopEvent = () => {
-      e.preventDefault()
-      e.stopPropagation()
+      e.preventDefault();
+      e.stopPropagation();
     }
     if (e.shiftKey) {
-      const video = videoInView()
+      const video = videoInView();
       if (! video) {
-        return
+        return;
       }
       switch (e.key) {
         case '{':
-          stopEvent()
-          video.playbackRate -= 0.05
-          showInfo(video, `rate: ${video.playbackRate.toFixed(2)}`)
-          return
+          stopEvent();
+          video.playbackRate -= 0.05;
+          showInfo(video, `rate: ${video.playbackRate.toFixed(2)}`);
+          return;
         case '}':
-          stopEvent()
-          video.playbackRate += 0.05
-          showInfo(video, `rate: ${video.playbackRate.toFixed(2)}`)
-          return
+          stopEvent();
+          video.playbackRate += 0.05;
+          showInfo(video, `rate: ${video.playbackRate.toFixed(2)}`);
+          return;
         case 'Backspace':
-          stopEvent()
-          video.playbackRate = 1
-          showInfo(video, `rate: ${video.playbackRate.toFixed(2)}`)
-          return
+          stopEvent();
+          video.playbackRate = 1;
+          showInfo(video, `rate: ${video.playbackRate.toFixed(2)}`);
+          return;
+        case 'M':
+          stopEvent();
+          if (video !== document.pictureInPictureElement) {
+            video.requestPictureInPicture();
+          } else {
+            document.exitPictureInPicture();
+          }
+          return;
       }
     }
-  })
-})()
+  });
+})();
