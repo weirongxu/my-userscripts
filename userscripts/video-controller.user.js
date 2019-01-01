abd90f09072d11d948caf5c2dc33551d35f77f3c
Weirong Xu <weirongxu.raidou@gmail.com>
2019-01-02 03:51:41 +0800

    fullscreen

--- userscripts/video-controller.user.js
+++ userscripts/video-controller.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         video controller
 // @namespace    https://github.com/weirongxu/my-userscripts
-// @version      0.5.1
+// @version      0.6.0
 // @description  video controller
 // @author       Raidou
 // @match        *://*/*
@@ -11,9 +11,34 @@
 (function() {
   'use strict';
 
-  function setStyle($node, css) {
+  (function initStyle() {
+    const head = document.head;
+    const style = document.createElement('style');
+    style.type = 'text/css';
+    style.textContent = `
+      .video-controller-info {
+        background: rgba(0, 0, 0, 0.5);
+        color: white;
+        padding: 5px;
+        z-index: 1000000000;
+        border-radius: 5px;
+        font-size: 12px;
+        visibility: hidden;
+        position: fixed;
+        opacity: 0;
+        transition: all .5s;
+      }
+      .video-controller-info.video-controller-info-show {
+        opacity: 1;
+        visibility: visible;
+      }
+    `;
+    head.appendChild(style);
+  })();
+
+  function setStyle($elem, css) {
     Object.entries(css).forEach(([key, val]) => {
-      $node.style[key] = val;
+      $elem.style[key] = val;
     });
   }
 
@@ -22,52 +47,56 @@
     const rect = video.getBoundingClientRect();
     const cls = 'video-controller-info';
     const $roots = document.querySelectorAll('body, :-webkit-full-screen');
-    const $root = $roots[$roots.length-1];
+    const $root = $roots[$roots.length - 1];
     let $info = $root.querySelector(`.${cls}`);
-    if (! $info) {
+    if (!$info) {
       $root.insertAdjacentHTML('beforeend', `<div class="${cls}"></div>`);
       $info = $root.querySelector(`.${cls}`);
     }
-    setStyle($info, {
-      background: 'rgba(0, 0, 0, 0.5)',
-      color: 'white',
-      padding: '5px',
-      zIndex: 1000000000,
-      borderRadius: '5px',
-      fontSize: '12px',
-      visibility: 'hidden',
-      position: 'fixed',
-      opacity: 0,
-      transition: '.5s opacity',
-    });
     $info.innerHTML = info;
-    $info.style.left = rect.left + video.clientWidth/2 - $info.clientWidth/2 + 'px';
-    $info.style.top = rect.top + video.clientHeight/2 - $info.clientHeight/2 + 'px';
-    $info.style.opacity = 1;
-    $info.style.visibility = 'visible';
+    setStyle($info, {
+      left: rect.left + video.clientWidth / 2 - $info.clientWidth / 2 + 'px',
+      top: rect.top + video.clientHeight / 2 - $info.clientHeight / 2 + 'px'
+    });
+    $info.classList.add('video-controller-info-show');
     clearTimeout(infoTimer);
     infoTimer = setTimeout(() => {
-      $info.style.opacity = 0;
-      $info.style.visibility = 'hidden';
+      $info.classList.remove('video-controller-info-show');
     }, 600);
   }
 
-  function inView($doms) {
+  function closest($elem, selector, _default = null) {
+    let matches;
+    if (typeof selector === 'string') {
+      matches = $elem => $elem.matches(selector);
+    } else if (typeof selector === 'function') {
+      matches = selector;
+    }
+    while (!matches($elem)) {
+      $elem = $elem.parentElement;
+      if (!$elem) {
+        return _default;
+      }
+    }
+    return $elem;
+  }
+
+  function inView($elems) {
     let maxVisibleArea = 0;
-    let winningDom = null;
-    $doms.forEach((dom) => {
-      const rect = dom.getBoundingClientRect();
+    let winningElem = null;
+    $elems.forEach(elem => {
+      const rect = elem.getBoundingClientRect();
       const left = Math.max(rect.left, 0);
       const top = Math.max(rect.top, 0);
       const right = Math.min(rect.right, window.innerWidth);
       const bottom = Math.min(rect.bottom, window.innerHeight);
-      const visibleArea = (right-left) * (bottom-top);
+      const visibleArea = (right - left) * (bottom - top);
       if (visibleArea > maxVisibleArea) {
         maxVisibleArea = visibleArea;
-        winningDom = dom;
+        winningElem = elem;
       }
     });
-    return winningDom;
+    return winningElem;
   }
 
   function videoInView() {
@@ -78,16 +107,25 @@
     return inView(document.querySelectorAll('iframe'));
   }
 
+  function click($elem) {
+    if ($elem) {
+      $elem.click();
+    }
+  }
+
   function eventTrigger(eventName) {
     const video = videoInView();
-    if (! video) {
+    if (!video) {
       const iframe = iframeInView();
       if (iframe) {
-        iframe.contentWindow.postMessage({
-          videoController: {
-            eventName,
-          }
-        }, '*')
+        iframe.contentWindow.postMessage(
+          {
+            videoController: {
+              eventName
+            }
+          },
+          '*'
+        );
       }
       return;
     }
@@ -112,23 +150,42 @@
           document.exitPictureInPicture();
         }
         break;
+      case 'page fullscreen':
+        click(
+          document.querySelector(
+            [
+              '.bilibili-player-video-btn.bilibili-player-video-btn-fullscreen [name=web_fullscreen]'
+            ].join(',')
+          )
+        );
+        break;
+      case 'fullscreen':
+        click(
+          document.querySelector(
+            [
+              '.bilibili-player-video-btn.bilibili-player-video-btn-fullscreen [name=browser_fullscreen]',
+              '.ytp-fullscreen-button'
+            ].join(',')
+          )
+        );
+        break;
     }
   }
 
-  window.addEventListener('message', (event) => {
+  window.addEventListener('message', event => {
     if (event.data.videoController && event.data.videoController.eventName) {
       eventTrigger(event.data.videoController.eventName);
     }
   });
 
-  document.addEventListener('keydown', (e) => {
+  document.addEventListener('keydown', e => {
     if (['INPUT', 'TEXTAREA'].includes(e.target.tagName)) {
       return;
     }
     const stopEvent = () => {
       e.preventDefault();
       e.stopPropagation();
-    }
+    };
     if (e.shiftKey) {
       switch (e.key) {
         case '{':
@@ -146,6 +203,14 @@
         case '"':
           stopEvent();
           eventTrigger('picture in picture');
+          return;
+        case 'Enter':
+          stopEvent();
+          if (e.ctrlKey) {
+            eventTrigger('fullscreen');
+          } else {
+            eventTrigger('page fullscreen');
+          }
           return;
       }
     }
